0d54e972afbad2edec36af2917ea8b33a5b6b061
Andrew Moskwoitz <andrewmoskowitz@gmail.com>
2016-12-15 11:54:27 -0500

    Updated Classes

--- 1st Project - New To Do List - Andrew Moskowitz/DataModel.swift
+++ 1st Project - New To Do List - Andrew Moskowitz/DataModel.swift
@@ -10,21 +10,56 @@ import Foundation
 
 var lists = [List]()
 
-class List {
+class List: NSObject, NSCoding {
+    
+    private struct Keys {
+        static let toDoListName = "toDoListName"
+        static let items = "items"
+    }
     var toDoListName: String
     var items = [Item]()
     
-    init(toDoListName: String) {
+    init(toDoListName: String, items: [Item]) {
         self.toDoListName = toDoListName
+        self.items = items
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        self.init(
+            toDoListName: aDecoder.decodeObject(forKey: Keys.toDoListName) as! String,
+            items: aDecoder.decodeObject(forKey: Keys.items) as! [Item]
+            )
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(toDoListName, forKey: Keys.toDoListName)
+        aCoder.encode(items, forKey: Keys.items)
     }
 }
 
-class Item {
+class Item: NSObject, NSCoding {
+    private struct Keys {
+        static let itemName = "itemName"
+        static let itemDescription = "itemDescription"
+    }
+    
     var itemName: String
     var itemDescription: String
     
     init(itemName: String, itemDescription: String) {
         self.itemName = itemName
         self.itemDescription = itemDescription
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        self.init(
+            itemName: aDecoder.decodeObject(forKey: Keys.itemName) as! String,
+            itemDescription: aDecoder.decodeObject(forKey: Keys.itemDescription) as! String
+            )
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(itemName, forKey: Keys.itemName)
+        aCoder.encode(itemDescription, forKey: Keys.itemDescription)
     }
 }
